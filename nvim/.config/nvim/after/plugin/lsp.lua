7d5af08d9dcb502a46155f93dbb267f530bf9f32
Kai Mies <develop@regnavi.de>
2023-02-23 13:34:44 +0100

    Disable stylua formatting in null-ls Creates weird formatting

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -77,7 +77,7 @@ null_ls.setup({
     end,
     sources = {
         null_ls.builtins.diagnostics.yamllint,
-        null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.flake8,
