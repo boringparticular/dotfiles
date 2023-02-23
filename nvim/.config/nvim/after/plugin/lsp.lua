cbad5b453fa43b5d960175cce7a124d028e3b0f8
Kai Mies <develop@regnavi.de>
2023-02-23 13:34:35 +0100

    Add formatting function using null-ls

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -59,6 +59,21 @@ local null_opts = lsp.build_options('null-ls', {})
 null_ls.setup({
     on_attach = function(client, bufnr)
         null_opts.on_attach(client, bufnr)
+
+        local format_cmd = function(input)
+            vim.lsp.buf.format({
+                id = client.id,
+                timeout_ms = 5000,
+                async = input.bang,
+            })
+        end
+
+        local bufcmd = vim.api.nvim_buf_create_user_command
+        bufcmd(bufnr, 'NullFormat', format_cmd, {
+            bang = true,
+            range = true,
+            desc = 'Format using null-ls'
+        })
     end,
     sources = {
         null_ls.builtins.diagnostics.yamllint,
