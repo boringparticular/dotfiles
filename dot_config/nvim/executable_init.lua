9b6119c2d62f76689cb8639b1a3a5719635e6398
Kai Mies <develop@regnavi.de>
2023-04-02 10:16:53 +0200

    Add lspsaga to neovim

--- dot_config/nvim/executable_init.lua
+++ dot_config/nvim/executable_init.lua
@@ -153,6 +153,20 @@ require('lazy').setup({
         end,
     },
     {
+        'glepnir/lspsaga.nvim',
+        opt = true,
+        branch = 'main',
+        event = 'LspAttach',
+        config = function()
+            require('lspsaga').setup({})
+        end,
+        requires = {
+            { 'nvim-tree/nvim-web-devicons' },
+            --Please make sure you install markdown and markdown_inline parser
+            { 'nvim-treesitter/nvim-treesitter' }
+        }
+    },
+    {
         'nvim-treesitter/nvim-treesitter',
         event = 'BufReadPost',
         build = ':TSUpdate',
