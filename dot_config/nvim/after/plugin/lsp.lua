46361df03cf387f4b0e5c02967d98d920c246d3b
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2023-05-16 09:50:46 +0200

    Update .config/nvim/after/plugin/lsp.lua

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -2,6 +2,7 @@ local lsp = require('lsp-zero').preset({
     name = 'recommended',
     set_lsp_keymaps = false,
     manage_nvim_cmp = true,
+    manage_nvim_cmp.set_extra_mappings = true,
     suggest_lsp_servers = true,
 })
 
