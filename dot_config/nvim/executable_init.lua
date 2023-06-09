a2fb845c3e233f0d35ed4fff45e9f0f332098dfa
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2023-06-09 15:52:16 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/executable_init.lua
+++ dot_config/nvim/executable_init.lua
@@ -161,6 +161,13 @@ require('lazy').setup({
         },
     },
     {
+        'L3MON4D3/LuaSnip',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
+    {
         'folke/tokyonight.nvim',
         lazy = false,
         priority = 1,
