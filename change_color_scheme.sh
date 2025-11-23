557159658437015ef5db6462ae1b7023a89d5eba
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-11-23 13:58:21 +0100

    feat: use elixir script instead of python for changing the color scheme

--- change_color_scheme.sh
+++ change_color_scheme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-./apply_color_scheme.py $(fd '*.yaml' schemes --glob | fzf --reverse --preview=window=default --preview='./color_preview.py {}')
+./apply_color_scheme.py $(fd '*.yaml' schemes --glob | fzf --reverse --preview=window=default --preview='./color_preview.exs {}')
 ./derive_color_formats.py
 chezmoi apply
